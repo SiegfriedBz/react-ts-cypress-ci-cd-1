7f71f4dae2050cf2aaa6334691e67b96fb0557f5
Siegfried <siegfried.bozza@renuo.ch>
2023-05-23 13:57:11 +0200

    fix ts linters...

--- src/contextAPI/contexts/todoContext.tsx
+++ src/contextAPI/contexts/todoContext.tsx
@@ -43,7 +43,7 @@ export const TodoContext = createContext<TodoContextType>({
   }
 })
 
-export const useTodoContext = () => {
+export const useTodoContext = (): TodoContextType => {
   return useContext(TodoContext)
 }
 
