a9a3dca2dec16f3b36d7a1cb942b8c9b247af490
Siegfried <siegfried.bozza@renuo.ch>
2023-05-23 19:47:18 +0200

    refactor todoContext.tsx

--- src/contextAPI/contexts/todoContext.tsx
+++ src/contextAPI/contexts/todoContext.tsx
@@ -1,46 +1,32 @@
 import React, { createContext, useContext, useReducer } from 'react'
 import { todoReducer } from '../reducers/todoReducer';
-import {v4 as uuidv4} from 'uuid';
-
-interface ITodo {
-  id: string;
-  content: string;
-  isDone: boolean;
-}
+import { v4 as uuidv4 } from 'uuid';
 
 interface IState {
-  todos: ITodo[];
+  todos: {
+    id: string;
+    content: string;
+    isDone: boolean;
+  }[];
   isEditMode: boolean;
 }
 
-type TodoContextType = {
-    todos: ITodo[];
-    isEditMode: boolean;
-    addTodo: (content: string) => void;
-    updateTodoContent: (id: string, content: string) => void;
-    toggleTodoIsDone: (id: string) => void;
-    deleteTodo: (id: string) => void;
-    toggleIsEditMode: () => void;
+interface TodoContextType extends IState {
+  addTodo: (content: string) => void;
+  updateTodoContent: (id: string, content: string) => void;
+  toggleTodoIsDone: (id: string) => void;
+  deleteTodo: (id: string) => void;
+  toggleIsEditMode: () => void;
 }
 
 export const TodoContext = createContext<TodoContextType>({
   todos: [],
   isEditMode: false,
-  addTodo: () => {
-    // do smthg //
-  },
-  updateTodoContent: () => {
-    // do smthg //
-  },
-  toggleTodoIsDone: () => {
-    // do smthg //
-  },
-  deleteTodo :() => {
-    // do smthg //
-  },
-  toggleIsEditMode: () => {
-    // do smthg //
-  }
+  addTodo: () => { /* dispatch */ },
+  updateTodoContent: () => { /* dispatch */ },
+  toggleTodoIsDone: () => { /* dispatch */ },
+  deleteTodo: () => { /* dispatch */ },
+  toggleIsEditMode: () => { /* dispatch */ }
 })
 
 export const useTodoContext = (): TodoContextType => {
@@ -60,24 +46,24 @@ export const TodoContextProvider = ({ children }: IProps): React.JSX.Element => 
 
   const [state, dispatch] = useReducer(todoReducer, initialState)
 
-  const addTodo = (content: string): void => {
-    const newTodo: ITodo = { id: uuidv4(), content, isDone: false }
+  const addTodo: TodoContextType['addTodo'] = (content) => {
+    const newTodo = { id: uuidv4(), content, isDone: false }
     dispatch({ type: 'ADD_TODO', payload: newTodo })
   }
 
-  const updateTodoContent = (id: string, content: string): void => {
+  const updateTodoContent: TodoContextType['updateTodoContent'] = (id, content) => {
     dispatch({ type: 'UPDATE_TODO_CONTENT', payload: { id, content } })
   }
 
-  const deleteTodo = (id: string): void => {
+  const deleteTodo: TodoContextType['deleteTodo'] = (id) => {
     dispatch({ type: 'DELETE_TODO', payload: { id } })
   }
 
-  const toggleTodoIsDone = (id: string): void => {
+  const toggleTodoIsDone: TodoContextType['toggleTodoIsDone'] = (id) => {
     dispatch({ type: 'TOGGLE_TODO_IS_DONE', payload: { id } })
   }
 
-  const toggleIsEditMode = (): void => {
+  const toggleIsEditMode: TodoContextType['toggleIsEditMode'] = () => {
     dispatch({ type: 'TOGGLE_IS_EDIT_MODE' })
   }
 
