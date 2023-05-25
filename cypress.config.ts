e124a4fc69a644f28c18f9ddb7728d89433ecd31
Siegfried <siegfried.bozza@renuo.ch>
2023-05-25 11:15:12 +0200

    revert  cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  component: {
-    devServer: {
-      framework: 'create-react-app',
-      bundler: 'webpack'
-    }
-  }
-})
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
