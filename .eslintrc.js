f25d0c9766eab4d534e2a1c8f9221a9c006ca325
Siegfried <siegfried.bozza@renuo.ch>
2023-05-25 10:00:31 +0200

    update  package.json + .eslintrc.js with cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,17 @@ module.exports = {
     es6: true,
     node: true,
     jest: true,
+    'cypress/globals': true
   },
   extends: [
     'react-app',
     'react-app/jest',
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:cypress/recommended'
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'cypress'],
   parserOptions: {
     ecmaVersion: 2017,
     sourceType: 'module',
@@ -29,6 +31,12 @@ module.exports = {
     ],
     '@typescript-eslint/explicit-function-return-type': 'warn', // Consider using explicit annotations for object literals and function return types even when they can be inferred.
     'no-empty': 'warn',
+    'cypress/no-assigning-return-values': 'error',
+    'cypress/no-unnecessary-waiting': 'error',
+    'cypress/assertion-before-screenshot': 'warn',
+    'cypress/no-force': 'warn',
+    'cypress/no-async-tests': 'error',
+    'cypress/no-pause': 'error'
   },
 };
 
